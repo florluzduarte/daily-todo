1ca7041f1553d69e96d3e8dfa5fcc003a6cc0f0a
Florencia Luz Duarte <florluzduarte@gmail.com>
2022-06-21 21:51:30 -0300

    Inicio del proyecto todo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { saludar } from "./js/componentes.js";
 import "./styles.css";
+import { Todo } from "./classes/todo.class.js";
 
-const nombre = "Florencia";
-
-saludar(nombre);
+const tarea = new Todo("Aprender Javascript");
+console.log(tarea);
