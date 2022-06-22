ea47e9d2c67ed58fe32fd1fee8fe39e672e861d1
Florencia Luz Duarte <florluzduarte@gmail.com>
2022-06-22 17:49:55 -0300

    Creación de tareas desde el input de la app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import "./styles.css";
 import { Todo, TodoList } from "./classes/index.class.js";
 import { crearTareaEnHtml } from "./js/componentes.js";
 
-const tarea = new Todo("Aprender Javascript");
-const listaDeTareas = new TodoList();
-listaDeTareas.nuevoTodo(tarea);
-crearTareaEnHtml(tarea);
-console.log(listaDeTareas);
+// const tarea = new Todo("Aprender Javascript");
+export const listaDeTareas = new TodoList();
+// listaDeTareas.nuevoTodo(tarea);
+// crearTareaEnHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,13 +1,20 @@
 // Esta constante referencia al div donde se encuentran mi lista completa de tareas
+import { Todo } from "../classes/index.class.js";
+import { listaDeTareas } from "../index.js";
+
+//Referencias al HTML
 const divTodoHtml = document.querySelector(".todo-list");
+const txtInput = document.querySelector(".new-todo");
 
 export const crearTareaEnHtml = (tarea) => {
   //Esta constante almacena los datos interpolados que vienen de mi clase Todo e inserta
   //cada dato dentro de la estructura HTML de una tarea nueva
   const tareaHtml = `
-  <li class="${tarea.completado}" data-id="${tarea.id}">
+  <li class="${tarea.completado ? "completed" : ""}" data-id="${tarea.id}">
     <div class="view">
-      <input class="toggle" type="checkbox" checked>
+      <input class="toggle" type="checkbox" ${
+        tarea.completado ? "checked" : ""
+      }>
       <label>${tarea.tarea}</label>
       <button class="destroy"></button>
     </div>
@@ -21,7 +28,23 @@ export const crearTareaEnHtml = (tarea) => {
 
   //De esta forma insertamos la tarea nueva en la lista de tareas que definimos al inicio
   //Luego esta funcionalidad estará separada en otra función
-  divTodoHtml.append(divTarea);
+  divTodoHtml.append(divTarea.firstElementChild);
 
-  return divTarea;
+  return divTarea.firstElementChild;
 };
+
+// Eventos
+
+txtInput.addEventListener("keyup", (event) => {
+  //Este if evalúa si al escribir en el input, la última tecla presionada es el enter
+  // keyCode: 13 equiavale al enter. Cada tecla tiene su keycode, y esa info se guarda en el evento
+  // que estamos escuchando. Por otro lado, también evalúa e impide que haya inputs sin texto.
+  if (event.keyCode === 13 && txtInput.value.length > 0) {
+    const tarea = new Todo(txtInput.value);
+    listaDeTareas.nuevoTodo(tarea);
+    crearTareaEnHtml(tarea);
+    console.log(listaDeTareas);
+    //Igualar el value a un string vacío equivale a borrar lo que escribimos en el input al dar enter
+    txtInput.value = "";
+  }
+});
